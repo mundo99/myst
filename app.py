2cba35fe5f1e177636ceeb02b5dbb8a89d8f4c8c
Doyeon Moon <83085413+mundo99@users.noreply.github.com>
2023-05-16 11:17:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x*x)
+
+st.title("하이하이")
+st.markdown("hihi")
+st.code("x=2021")
+st.latex(r'''
+  a + a r ^1+ a r^2 + r^3 ''')
